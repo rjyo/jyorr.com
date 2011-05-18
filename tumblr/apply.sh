6e2c70a7d6489dc92f64baf0b70a3903c74c7730
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2011-05-18 12:11:54 +0900

    update hp

--- tumblr/apply.sh
+++ tumblr/apply.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-CMD="scp `dirname $0`/screen.css jyo@xuxiaoyu.com:~/jyorr/public/css"
+# CMD="scp `dirname $0`/screen.css jyo@xuxiaoyu.com:~/jyorr/public/css"
+# echo $CMD
+# `$CMD`
+
+# CMD="scp `dirname $0`/jyorr.js jyo@xuxiaoyu.com:~/jyorr/public/js"
+# echo $CMD
+# `$CMD`
+
+CMD="rsync -r public/ jyo@xuxiaoyu.com:~/jyorr/public/"
 echo $CMD
 `$CMD`
 
-CMD="scp `dirname $0`/jyorr.js jyo@xuxiaoyu.com:~/jyorr/public/js"
-echo $CMD
-`$CMD`
